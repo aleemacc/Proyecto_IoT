0e819bf920a83d790f91983f8aebc05885d6b95d
aleemacc <alejandromacias2005@protonmail.com>
2025-12-21 18:03:05 +0100

    base implementacion interrupcion

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//GPIO pins que podemos usar: D1, D2, D5, D6, D7, A0(solo analog read)
+
+const int pinAbrir = 1;
+const int pinCerrar = 2;
+const int pinInterrupcion = 7;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(pinInterrupcion, INPUT_PULLUP); //usamos la resistencia pullup integrada en la placa
+  attachInterrupt(pinInterrupcion, isr, FALLING); //Esto define una interrupcion, el primer valor es el pin que se observa, el segundo es la funcion a la que se llama en caso de la interrupcion, y el tercer valor es en que evento del pin llama a la funcion.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void ICACHE_RAM_ATTR isr(){
+  //Esta sera la funcion llamada para la interrupcion cuando se pulse un interruptor limite
 }
 
 // put function definitions here:
